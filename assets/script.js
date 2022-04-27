2e16d2c6ea890961c6290b8cc55e61c9f84fd628
Nihar Gavva <nihargavva@gmail.com>
2022-04-27 16:55:19 +0000

    still working on it

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+let plusButton = document.querySelector("#quantity-up")
+let minusButton = document.querySelector("#quantity-down")
+let counter = 1
+let counterDisplayElem = document.querySelector(".total-quantity")
+plusButton.addEventListener('click',()=> {
+  counter++
+  updateDisplay();
+})
+minusButton.addEventListener('click',()=> {
+  if(counter > 1)
+  {
+    counter--
+  updateDisplay();
+  }
+})
+function updateDisplay()
+{
+  counterDisplayElem.innerHTML = "Quantity: " + counter;
+}
